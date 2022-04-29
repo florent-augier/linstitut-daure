570552ed11c8293f9fdc508a747ed664239ac9e9
florent <florent.sauvetage@gmail.com>
2022-04-29 16:27:07 +0200

    Fix(images): resize, define size

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -137,14 +137,14 @@ export default function Home() {
   const dataCard = [
     {
       imgUrl:
-        "https://res.cloudinary.com/flowww-dev/image/upload/v1647431087/L%27institut%20d%27Aur%C3%A9/Extensions-de-cils_sk3zwu.webp",
+        "https://res.cloudinary.com/flowww-dev/image/upload/c_scale,q_100,w_660/v1647431087/L'institut%20d'Aur%C3%A9/Extensions-de-cils_sk3zwu.webp",
       title: "EXTENSION DE CILS",
       body: "Créez des cils qui sublimeront votre regard",
       footerLink: "/cils",
     },
     {
       imgUrl:
-        "https://res.cloudinary.com/flowww-dev/image/upload/v1647431588/L%27institut%20d%27Aur%C3%A9/Micropigmentation_jckwjc.webp",
+        "https://res.cloudinary.com/flowww-dev/image/upload/c_scale,q_100,w_230/v1647431588/L'institut%20d'Aur%C3%A9/Micropigmentation_jckwjc.webp",
       title: "MICROPIGMENTATION",
       body: "Dessinez votre sourcils afin de lui redonner un forme homogène",
       footerLink: "/micropigmentation",
@@ -152,14 +152,14 @@ export default function Home() {
 
     {
       imgUrl:
-        "https://res.cloudinary.com/flowww-dev/image/upload/v1647431187/L%27institut%20d%27Aur%C3%A9/Dermopigmentation_pny4sp.webp",
+        "https://res.cloudinary.com/flowww-dev/image/upload/c_scale,q_100,w_230/v1647431187/L'institut%20d'Aur%C3%A9/Dermopigmentation_pny4sp.webp",
       title: "DERMOPIGMENTATION",
       body: "Modelez vos sourcils ou lèvres dans le but de redonner du volume",
       footerLink: "/dermopigmentation",
     },
     {
       imgUrl:
-        "https://res.cloudinary.com/flowww-dev/image/upload/v1647442043/L%27institut%20d%27Aur%C3%A9/Soins-visage_hn4gvq.webp",
+        "https://res.cloudinary.com/flowww-dev/image/upload/c_scale,q_100,w_230/v1647442043/L'institut%20d'Aur%C3%A9/Soins-visage_hn4gvq.webp",
       title: "SOINS CORPORELS",
       body: "Dessinez votre sourcils afin de lui redonner ",
       footerLink: "/soins-corporels",
@@ -174,6 +174,8 @@ export default function Home() {
       imgSrc:
         "https://res.cloudinary.com/flowww-dev/image/upload/v1649426541/L%27institut%20d%27Aur%C3%A9/Extension-de-cils_l0divs_rtsoel.webp",
       imgAlt: "technique d'extension de cil",
+      width: "960px",
+      height: "540px",
     },
     {
       title: "Un savoir faire unique",
@@ -183,6 +185,8 @@ export default function Home() {
         "https://res.cloudinary.com/flowww-dev/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1647431187/L%27institut%20d%27Aur%C3%A9/Dermopigmentation_pny4sp.webp",
 
       imgAlt: "technique de BB glow | dermopigmentation",
+      width: "1000px",
+      height: "563px",
     },
     {
       title: "Un rapport qualité/prix imbattable",
@@ -191,6 +195,8 @@ export default function Home() {
       imgSrc:
         "https://res.cloudinary.com/flowww-dev/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1647431588/L%27institut%20d%27Aur%C3%A9/Micropigmentation_jckwjc.webp",
       imgAlt: "technique d'extension microblading | micropigmentation",
+      width: "1000px",
+      height: "563px",
     },
   ];
 
@@ -216,7 +222,12 @@ export default function Home() {
           return (
             <div className={`card-grid ${index}`} key={index.toString()}>
               <div className="image-card-grid">
-                <img src={card.imgUrl} alt={card.title} />
+                <img
+                  src={card.imgUrl}
+                  alt={`Présentation de la prestation: ${card.title}`}
+                  width="230px"
+                  height="230px"
+                />
               </div>
               <div className="title-card-grid">{card.title}</div>
               <div className="body-card-grid">{card.body}</div>
@@ -242,9 +253,9 @@ export default function Home() {
             <div className="container-img">
               <img
                 src={container.imgSrc}
-                alt="Présentation de l'institut d'Auré"
-                width="6720px"
-                height="4480px"
+                alt={container.imgAlt}
+                width={container.imgWidth}
+                height={container.imgHeight}
               />
             </div>
           </div>
